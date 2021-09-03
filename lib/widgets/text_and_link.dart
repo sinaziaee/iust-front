6469e6ee6fe90cfe34a20d06fba859d556d47da2
sinaziaee <sinaziaee99@gmail.com>
2021-09-03 18:45:52 +0430

    added responsivness to course material page

--- lib/widgets/text_and_link.dart
+++ lib/widgets/text_and_link.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import '../constants.dart';
 
@@ -6,35 +7,73 @@ class HyperLinkText extends StatelessWidget {
   final String hyperText;
   final String text;
   final VoidCallback onTapped;
+  final Size size;
 
   HyperLinkText({
     required this.text,
     required this.onTapped,
     required this.hyperText,
+    required this.size,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(
-          '- ',
-          style: kBlackTextStyle.copyWith(),
-        ),
-        InkWell(
-          onTap: onTapped,
-          child: Text(
-            hyperText,
-            style: kBlackTextStyle.copyWith(
-              color: Colors.blue,
+    if (size.width > 720) {
+      return Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                '- ',
+                style: kBlackTextStyle.copyWith(),
+              ),
+              InkWell(
+                onTap: onTapped,
+                child: Text(
+                  hyperText,
+                  style: kBlackTextStyle.copyWith(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              Container(
+                child: Text(
+                  text,
+                  style: kBlackTextStyle.copyWith(),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+        ],
+      );
+    } //
+    else {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 3,
+          ),
+          InkWell(
+            onTap: onTapped,
+            child: Text(
+              hyperText,
+              style: kBlackTextStyle.copyWith(
+                color: Colors.blue,
+              ),
             ),
           ),
-        ),
-        Text(
-          text,
-          style: kBlackTextStyle.copyWith(),
-        ),
-      ],
-    );
+          Container(
+            child: Text(
+              text,
+              style: kBlackTextStyle.copyWith(),
+            ),
+          ),
+        ],
+      );
+    }
   }
 }
